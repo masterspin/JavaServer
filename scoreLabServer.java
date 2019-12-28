788d08dd0faa0982c479b4fdd6fa61fd907da499
masterspin <45522785+masterspin@users.noreply.github.com>
2019-12-28 15:24:45 -0500

    Update scoreLabServer.java

--- scoreLabServer.java
+++ scoreLabServer.java
@@ -13,7 +13,7 @@ public class scoreLabServer {
         System.out.println("Connecting to port 8080");
         while (true) {
             try (Socket socket = server.accept()) {
-                String text = "ritz363 loves contributing to ScoreLab";
+                String text = "HTTP/1.1 200 OK\r\n\r\nritz363 loves contributing to ScoreLab";
                 socket.getOutputStream().write(text.getBytes("UTF-8"));                
             }
         }
